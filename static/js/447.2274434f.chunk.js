102866917cb541e1015d5a7728bb4eaa6bf25151
Dmitriy <tiemposilencioo@gmail.com>
2023-09-18 19:06:03 +0300

    Updates

--- static/js/447.2274434f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_task_dial_alliance=self.webpackChunktest_task_dial_alliance||[]).push([[447],{447:function(e,a,n){n.r(a),n.d(a,{default:function(){return c}});var s=n(356),t=n(184),c=function(){return(0,t.jsx)("section",{className:"reportPage",children:(0,t.jsx)("div",{className:"container",children:(0,t.jsx)(s.Z,{boolean:!1,countView:1/0,slice:1})})})}}}]);
-//# sourceMappingURL=447.2274434f.chunk.js.map
